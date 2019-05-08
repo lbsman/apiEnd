0d42e1dc8149d8721b7b6a7485ff42f1669ada8b
Josh r <jstwrenn@gmail.com>
2019-05-07 23:04:16 -0600

    Setup for heroku

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 const func = require('./postToInex.js');
 const axios = require('axios');
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.set('views', __dirname + '/views');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -14,7 +15,7 @@ app.get('/',(req, res) => {
 
 app.post('/Send', (req, res) => {
     var url = 'https://api.inexusdialer.com/iNexusSoap/Service.asmx/ImportLeadBatch?tenantID=237';
-    axios.post(url,{"body": func.nexPost(req.body), "Authorization": "Basic "}).then((response) => {
+    axios.post(url,{"body": func.nexPost(req.body), "Authorization": "Basic " + new Buffer("JoshWrenn:xq*QV2_z").toString('base64')}).then((response) => {
         console.log(response);
     }).then((reponse) => {
         res.redirect('/thankYou.html');
@@ -27,6 +28,6 @@ app.get('/thankYou.html', (req, res) => {
     res.render('thankYou.html');
 });
 
-app.listen(3000, () => {
-    console.log('Started on port 3000');
+app.listen(port, () => {
+    console.log('Started on port ' + port);
 });
